df6ea540c7d5fe673b2707dc40c8cf332f0647d4
renx777 <arenukaprasad@gmail.com>
2017-12-07 02:01:57 +0530

    imported and added files from codepen

---
+++ myjs.js
@@ -0,0 +1,59 @@
+
+function delf(){
+    var j=document.getElementById("songTextInput").value;
+    var o=j.split("");
+    o.pop();
+    document.getElementById("songTextInput").value=o.join("");
+    
+    
+    
+    }
+    
+    
+    function cls(num){
+    
+    document.getElementById("songTextInput").value="";
+    
+    }
+    
+    function dis(num){
+    if(num==17){
+    
+    num="*0";
+    
+    }
+    
+    
+    if(num==11){
+    num="+";
+    
+    }
+    if(num==12){
+    num="-";
+    }
+    if(num==13){
+    num="*"
+    }
+    if(num==14){
+    num="π";
+    }
+    if(num===15){
+    num="*(";
+    }
+    if(num===16){
+    num=")"}
+    var k=num;
+    var l=num;
+    document.getElementById("songTextInput").value=document.getElementById("songTextInput").value+num;
+    
+    }
+    
+    function eva(){
+    
+    var k=document.getElementById("songTextInput").value;
+     var t= k.replace('π','3.14159')
+    var l=eval(t);
+    document.getElementById("songTextInput").value=l.toFixed(2);
+    
+    }
+    
